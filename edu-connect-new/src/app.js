68dcb816ccec5f7c77669cdb61a337300e488b63
oa180 <oa20180180@gmail.com>
2026-01-28 18:48:01 +0200

    swagger fix

--- edu-connect-new/src/app.js
+++ edu-connect-new/src/app.js
@@ -19,14 +19,24 @@ app.use(cors({
 app.options('*', cors())
 app.use(express.json({ limit: '1mb' }))
 app.use(morgan('combined'))
-
+app.set('trust proxy', true);
 app.use('/api', routes)
 if (process.env.SWAGGER_ENABLED !== 'false') {
-  app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(null, { swaggerUrl: '/api/docs.json' }))
+  // Use a relative path that works with the browser URL
+  app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(null, { 
+    swaggerOptions: {
+      url: '/api/docs.json' 
+    }
+  }));
+
   app.get('/api/docs.json', (req, res) => {
-    const dynamicSpec = { ...openapiSpec, servers: [{ url: `${req.protocol}://${req.get('host')}/api` }] }
-    res.json(dynamicSpec)
-  })
+    // This will now correctly show https://yourdomain.com/api or http://76.13.102.118/api
+    const dynamicSpec = { 
+      ...openapiSpec, 
+      servers: [{ url: `${req.protocol}://${req.get('host')}/api` }] 
+    };
+    res.json(dynamicSpec);
+  });
 }
 app.use(errorHandler)
 
