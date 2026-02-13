a215dedb8eee9c118a572543e5c72b609d72074d
oa180 <oa20180180@gmail.com>
2026-02-13 19:03:53 +0200

    Allowing Get Banner From any where

--- src/modules/banner/routes.js
+++ src/modules/banner/routes.js
@@ -40,11 +40,10 @@ const upload = multer({
   limits: { fileSize: 5 * 1024 * 1024 }
 })
 
-router.use(auth)
-router.post('/', upload.single('image'), controller.uploadBanner)
-router.get('/', controller.listBanners)
-router.get('/:id', [param('id').isInt()], controller.getBanner)
-router.get('/:id/image', [param('id').isInt()], controller.getBannerImage)
-router.delete('/:id', [param('id').isInt()], controller.deleteBanner)
+router.get('/', auth, controller.listBanners)
+router.get('/:id', auth, [param('id').isInt()], controller.getBanner)
+router.get('/:id/image', auth, [param('id').isInt()], controller.getBannerImage)
+router.post('/', auth, upload.single('image'), controller.uploadBanner)
+router.delete('/:id', auth, [param('id').isInt()], controller.deleteBanner)
 
 module.exports = router
